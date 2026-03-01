450986b44500c4937807ac61ce10d520f04b22bb
Marciah Malangwane <marciah.malangwane@capaciti.org.za>
2026-03-01 20:00:58 +0200

    Add booking routes to the main application file

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes.js";
 import paymentRoutes from "./modules/payments/payments.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import bookingRoutes from "./modules/bookings/bookings.routes.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.get("/", (_, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 app.use(errorHandler);
 
