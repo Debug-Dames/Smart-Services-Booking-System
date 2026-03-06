d4218c1edbacb30bd6f1f10c73b2d5650fcd7c8a
comfortN <nqobilecomfyngwenya@gmail.com>
2026-03-06 11:40:35 +0200

    updating the ES Modules for tests

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,7 +1,14 @@
+// module.exports = {
+//   testEnvironment: "node",
+//   transform: {
+//     "^.+\\.js$": "babel-jest"
+//   },
+//   setupFiles: ["<rootDir>/tests/setupTests.js"], 
+// };
+
 module.exports = {
   testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest"
-  },
-  setupFiles: ["<rootDir>/tests/setupTests.js"], 
+  transform: {},
+  testMatch: ["**/tests/**/*.test.js"],
+  setupFiles: ["<rootDir>/tests/setupTests.js"]
 };
