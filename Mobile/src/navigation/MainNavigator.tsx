c99bef8ec334d279100f72fa6cff3ef91d83f857
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2026-03-09 04:04:37 +0200

    Updating the app routing

--- Mobile/src/navigation/MainNavigator.tsx
+++ Mobile/src/navigation/MainNavigator.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/home/HomeScreen';
+import TabNavigator from './TabNavigator';
+import BookingSuccessScreen from '../screens/booking/BookingSuccessScreen';
+import MyBookingsScreen from '../screens/booking/BookingSummaryScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen
+      {/* <Stack.Screen
         name="Home"
         component={HomeScreen}
         options={{ headerShown: false }}
+      /> */}
+      <Stack.Screen
+        name="Tabs"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="BookingSuccess"
+        component={BookingSuccessScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="MyBookings"
+        component={MyBookingsScreen}
       />
     </Stack.Navigator>
   );
