14438303b999bcae7e235199ffb56bbb466add33
Zamo797 <nomzamonhlap97@gmail.com>
2026-03-31 15:47:59 +0200

    admindata update 2.2

---
+++ backend/src/modules/admin/admin.controller.js
@@ -0,0 +1,11 @@
+import * as adminService from "./admin.service.js";
+
+export const getUsers = async (req, res) => adminService.getUsers(req, res);
+
+export const getServices = async (req, res) => adminService.getServices(req, res);
+
+export const createService = async (req, res) => adminService.createService(req, res);
+
+export const updateService = async (req, res) => adminService.updateService(req, res);
+
+export const deleteService = async (req, res) => adminService.deleteService(req, res);
