c5992b84fa20447bb77809af3d38a6b630c464e4
Zamo797 <nomzamonhlap97@gmail.com>
2026-04-02 13:32:13 +0200

    updating link removing localhost

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000",
+        url: "https://smart-services-booking-system-backend-uzip.onrender.com",
       },
     ],
     components: {
